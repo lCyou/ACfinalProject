d2be165f6fabdec9a1edaa771041f6ce1cf6b6e9
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2023-02-17 03:30:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 # db = SQL("sqlite:///application.db")
 
 # Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
+# if not os.environ.get("API_KEY"):
+#     raise RuntimeError("API_KEY not set")
 
 
 @app.after_request
